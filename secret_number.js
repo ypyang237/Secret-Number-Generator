ff9cbacd7d222d402423085225763d68b4f4925f
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-08 20:38:33 -1000

    day 2 hwk -2

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,24 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function SecretNumberGenerator() {
+  var min = 0;
+  var max = 1000000;
+  var num = null;
+
+
+    function getSecretNumber(){
+         if (num === null){
+            num = Math.floor(Math.random() * (max - min + 1) + min);
+
+        }
+
+            return num;
+
+    }
+
+        return getSecretNumber;
+
+
 };
+
+
+
